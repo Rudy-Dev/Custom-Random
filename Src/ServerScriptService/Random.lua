85038324265bd90cb937758fa7812bc6824e23fc
Wiktor Rudnicki <WiktorRudnicki.@mail>
2021-01-16 16:14:50 +0100

    Added alot functions

--- Src/ServerScriptService/Random.lua
+++ Src/ServerScriptService/Random.lua
@@ -1,32 +1,100 @@
-local Random = {}
+local CustomRandom = {}
 
-local pi = 3.141
+--Some basic stuff because yes
 
-function Random.Range(min, max)
-    local result = math.random(min, max)
+local pi = math.pi
+local StandartRandom = Random.new()
+
+--return value that is smaller than max but bigger than min and its a int
+
+function CustomRandom.RangeInt(min, max)
+    local result = StandartRandom:NextInteger(min, max)
+
+    return result
+end
+
+--return value that is smaller than max but bigger than min and its a float
+
+function CustomRandom.Range(min, max)
+    local result = StandartRandom:NextNumber(min, max)
+
+    return result
+end
+
+--return value that is smaller than 1 but bigger than 0 and its an int
+
+function  CustomRandom.valueInt()
+    local result = StandartRandom:NextInteger(0, 1)
+
+    return result
+end
+
+--return value that is smaller than 1 but bigger than 0 and its a float
+
+function  CustomRandom.value()
+    local result = StandartRandom:NextNumber(0, 1)
 
     return result
 end
 
 
-function Random.InsideUnitCircle()
-    local result = Vector2.new;
+--return vector2 point in a circle with radius 1
 
-    local M_PI = pi
+function CustomRandom.InsideUnitCircle()
 
-    local a = Random.Range(0, 1)
+    local a = CustomRandom.Range(0, 2 * pi)
+ 
+    local x = math.sqrt(CustomRandom.value()) * math.cos(a);
+    local y = math.sqrt(CustomRandom.value()) * math.sin(a);
 
-    local theta = 2 * M_PI * a;
-    local phi = math.Acos(1 * Random.Range(0, 1));
-    local x = math.Sin(phi) * math.Cos(theta);
-    local y = math.Sin(phi) * math.Sin(theta);
-    local z = math.Cos(phi);
-
-    result.x = x;
-    result.y = y;
+    local result = Vector2.new(x, y)
 
     return result
 end
 
+--return vector3 point in a sphere with radius 1
 
-return Random
+function CustomRandom.insideUnitSphere()
+    
+    local theta = CustomRandom.Range(-1, 1) * 2.0 * pi;
+    local phi = math.acos(2.0 * CustomRandom.Range(-1, 1) - 1.0);
+    local r = math.sqrt(CustomRandom.Range(-1, 1));
+
+    local sinTheta = math.sin(theta);
+    local cosTheta = math.cos(theta);
+    local sinPhi = math.sin(phi);
+    local cosPhi = math.cos(phi);
+
+    local x = r * sinPhi * cosTheta;
+    local y = r * sinPhi * sinTheta;
+    local z = r * cosPhi;
+
+    local result = Vector3.new(x, y, z)
+
+    return result
+end
+
+function CustomRandom.RandomColor3()
+    local result = Color3.new(CustomRandom.value(), CustomRandom.value(), CustomRandom.value())
+
+    return result
+end
+
+function CustomRandom.RandomMaterial()
+    local Materials = {}
+
+    for _,enum in pairs(Enum:GetEnums()) do
+        if enum == Enum.Material  then
+              for _,enumItem in pairs(enum:GetEnumItems()) do
+               
+               table.insert(Materials, enumItem.Value)
+           end
+        end 
+    end
+    local result = Materials[CustomRandom.RangeInt(1, #Materials)]
+
+    return result
+end
+
+return CustomRandom
+
--- Src/ServerScriptService/Test.server.lua
+++ Src/ServerScriptService/Test.server.lua
@@ -1,8 +1,30 @@
 local Random = require(script.Parent.Random)
+local is = game:GetService("UserInputService")
 
-for  i = 0, 10, 1  do
+is.InputBegan:Connect(function(key)
+    if  key.KeyCode == Enum.KeyCode.T then
+        bruh()
+    end
     
-    print(Random.InsideUnitCircle())
+end)
+    
 
+function bruh()
+     
+    for  i = 0, 2000, 1  do
+    
+
+        local a = Instance.new("Part")
+    
+        a.Anchored = true
+        a.Position = Random.insideUnitSphere() * 50
+        a.Parent = workspace
+        a.Shape = Enum.PartType.Ball
+        local b = Random.Range(1, 5)
+        a.Size = Vector3.new(b, b, b)
+        a.Color = Random.RandomColor3()
+        a.Material = Random.RandomMaterial()
+    
+    end
 end
 
