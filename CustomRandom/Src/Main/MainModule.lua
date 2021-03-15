8701cc3a8fdf06f869105717a63545fb16f70d25
Wiktor Rudnicki <WiktorRudnicki.@mail>
2021-03-15 15:25:44 +0100

    Added linear congruential generator
    Minor fixes

--- CustomRandom/Src/Main/MainModule.lua
+++ CustomRandom/Src/Main/MainModule.lua
@@ -3,117 +3,123 @@ local CustomRandom = {}
 --Some Useful Variables 
 
 local pi = math.pi
-local StandartRandom = Random.new()
+
+local x = 1664525
+local y = 1013904223
+local m = math.pow(2, 32)
+local seed = 12234;
+
+--Generate seed
+function CustomRandom.nextRand() 
+	seed = (x * seed + y) % m + os.clock();
+	return seed;
+end
 
 --Returns an int that is smaller than max but bigger than min
 function CustomRandom.RangeInt(min, max)
-    local result = StandartRandom:NextInteger(min, max)
+	local result = (CustomRandom.nextRand() % (max - min + 1)) + min;
 
-    return result
+	return result
 end
 
 --Returns a number that is smaller than max but bigger than min
 function CustomRandom.Range(min, max)
-    local result = StandartRandom:NextNumber(min, max)
+	local result = (CustomRandom.nextRand() % (max - math.pow(0.104231, 1.104231) - min + 1 - 1)) + min;
 
-    return result
+	return result
 end
 
 --Returns 0 or 1
 function  CustomRandom.valueInt()
-    local result = StandartRandom:NextInteger(0, 1)
+	local result = (CustomRandom.nextRand() % (1 - 0 + 1)) + 0;
 
-    return result
+	return result
 end
 
 --Returns a number that is smaller than 1 but bigger than 0
 function  CustomRandom.value()
-    local result = StandartRandom:NextNumber(0, 1)
+	local result = (CustomRandom.nextRand() % (1 - math.pow(0.104231, 1.104231) - 0 + 1 - 1)) + 0;
 
-    return result
+	return result
 end
 
 --Returns a point in a circle with a radius of 1
 function CustomRandom.InsideUnitCircle()
 
-    local a = CustomRandom.Range(0, 2 * pi)
- 
-    local x = math.sqrt(CustomRandom.value()) * math.cos(a);
-    local y = math.sqrt(CustomRandom.value()) * math.sin(a);
+	local a = CustomRandom.Range(0, 2 * pi)
 
-    local result = Vector2.new(x, y)
+	local x = math.sqrt(CustomRandom.value()) * math.cos(a);
+	local y = math.sqrt(CustomRandom.value()) * math.sin(a);
 
-    return result
+	local result = Vector2.new(x, y)
+
+	return result
 end
 
 --Returns a point in a sphere with a radius of 1
 function CustomRandom.insideUnitSphere()
-    
-    local theta = CustomRandom.Range(-1, 1) * 2.0 * pi;
-    local phi = math.acos(2.0 * CustomRandom.Range(-1, 1) - 1.0);
-    local r = math.sqrt(CustomRandom.Range(-1, 1));
 
-    local sinTheta = math.sin(theta);
-    local cosTheta = math.cos(theta);
-    local sinPhi = math.sin(phi);
-    local cosPhi = math.cos(phi);
+	local theta = CustomRandom.Range(-1, 1) * 2.0 * pi;
+	local phi = math.acos(2.0 * CustomRandom.Range(-1, 1) - 1.0);
+	local r = math.sqrt(CustomRandom.Range(-1, 1));
 
-    local x = r * sinPhi * cosTheta;
-    local y = r * sinPhi * sinTheta;
-    local z = r * cosPhi;
+	local sinTheta = math.sin(theta);
+	local cosTheta = math.cos(theta);
+	local sinPhi = math.sin(phi);
+	local cosPhi = math.cos(phi);
 
-    local result = Vector3.new(x, y, z)
+	local x = r * sinPhi * cosTheta;
+	local y = r * sinPhi * sinTheta;
+	local z = r * cosPhi;
 
-    return result
+	local result = Vector3.new(x, y, z)
+
+	return result
 end
 
 --Returns a random Color3
 function CustomRandom.RandomColor3()
-    local result = Color3.new(CustomRandom.value(), CustomRandom.value(), CustomRandom.value())
+	local result = Color3.new(CustomRandom.value(), CustomRandom.value(), CustomRandom.value())
 
-    return result
+	return result
 end
 
 --Returns a random EnumItem.Value
 function CustomRandom.RandomMaterial()
-    local Materials = {}
+	local Materials = {}
 
-    for _,enum in pairs(Enum:GetEnums()) do
-        if enum == Enum.Material  then
-              for _,enumItem in pairs(enum:GetEnumItems()) do
-               table.insert(Materials, enumItem.Value)
-           end
-        end 
-    end
-    local result = Materials[CustomRandom.RangeInt(1, #Materials)]
+	for _,i in pairs(Enum.Material:GetEnumItems()) do
+		table.insert(Materials, i.Value)
+	end
+	local result = Materials[CustomRandom.RangeInt(1, #Materials)]
 
-    return result
+	return result
 end
 
 --Returns a random point in a cube with a size of 1
 function CustomRandom.InsideUnitCube()
-    
-    local x = CustomRandom.Range(-1, 1);
-    local y = CustomRandom.Range(-1, 1);
-    local z = CustomRandom.Range(-1, 1);
 
-    local result = Vector3.new(x, y, z)
+	local x = CustomRandom.Range(-1, 1);
+	local y = CustomRandom.Range(-1, 1);
+	local z = CustomRandom.Range(-1, 1);
 
-    return result
+	local result = Vector3.new(x, y, z)
+
+	return result
 end
 
 --Returns a random point on a sphere with a radius of 1
 function CustomRandom.OnUnitSphere()
-    local a = CustomRandom.Range(-1, 1)
-    local b = CustomRandom.Range(0, 2 * pi)
+	local a = CustomRandom.Range(-1, 1)
+	local b = CustomRandom.Range(0, 2 * pi)
 
-    local x = math.sqrt(1 - a ^ 2) * math.cos(b)
-    local y =  math.sqrt(1 - a ^ 2) * math.sin(b)
-    local z = a
+	local x = math.sqrt(1 - a ^ 2) * math.cos(b)
+	local y =  math.sqrt(1 - a ^ 2) * math.sin(b)
+	local z = a
 
-    local result = Vector3.new(x, y, z)
+	local result = Vector3.new(x, y, z)
 
-    return result
+	return result
 end
 
 return CustomRandom
